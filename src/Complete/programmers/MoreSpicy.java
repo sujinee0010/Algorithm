32047450cdb696a298e77ad8ee6fb2603487e259
sujin <sujinee9797@gmail.com>
2020-07-07 10:35:50 +0900

    200706_programmers_더맵게
    우선순위 큐 (수정)

--- src/Complete/programmers/MoreSpicy.java
+++ src/Complete/programmers/MoreSpicy.java
@@ -1,5 +1,5 @@
 package Complete.programmers;
-//프로그래머스 더맵게 
+//프로그래머스 더맵게
 import java.util.PriorityQueue;
 public class MoreSpicy {
     public int solution(int[] scoville, int K) {
@@ -7,20 +7,15 @@ public class MoreSpicy {
         PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();//숫자는 낮은 것이 우선순위를 높게
         for(int i=0;i<scoville.length;i++)
             priorityQueue.add(scoville[i]);
-        if(priorityQueue.stream().allMatch(x->x==0)) return 0;
-        while(!priorityQueue.isEmpty()){
-            if(priorityQueue.stream().allMatch(x->x>=K)) return answer;
-            if(priorityQueue.size()>=2) {
+        while(priorityQueue.size()>1 &&  priorityQueue.peek()<K){
                 int num1 = priorityQueue.poll();
                 int num2 = priorityQueue.poll();
                 int mixscore = num1 + (num2 * 2);
                 priorityQueue.add(mixscore);
                 answer++;
-            }else{
-                if(priorityQueue.stream().allMatch(x->x>=K)) return answer;
-                else return -1;
-            }
         }
+        if(priorityQueue.size()<=1 && priorityQueue.peek()<K) return -1;
+
         return answer;
     }
 }
