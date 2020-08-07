0cd4bfcce3cdc89935555456edb0c7f4e3335930
sujin <sujinee9797@gmail.com>
2020-08-08 01:42:53 +0900

    200808_programmers_등굣길
    DP/ 최단경로의 수

--- src/Complete/programmers/School.java
+++ src/Complete/programmers/School.java
@@ -1,73 +1,24 @@
 package Complete.programmers;
-
-import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
-
-import java.awt.*;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.PriorityQueue;
-
+//최단거리 수 , 대각선의 합 , DP,  프로그래머스 등굣길
 public class School {
-    static int road[][];  static boolean visited[][];
-    static int N,M , min;
-    static Point minp = new Point(0,0);
-    static ArrayList plist;
-    //static ArrayList lcntList = new ArrayList<Point>();
-    //static ArrayList llist = new ArrayList<Integer>();
-    static int dx[] ={-1,1,0,0}; static int dy[] ={0,0,-1,1};
-    public int solution(int m, int n, int[][] puddles) {
-        int answer = 0;
-        N = n; M = m;
-        // 제외 할 것 : 시작 , 웅덩이
-        road  = new int[m][n];
-        visited  = new boolean[m][n];
-        plist = new ArrayList<Point>();
-
-
-        for (int i=0;i<puddles.length;i++){
-          plist.add(new Point(puddles[i][0],puddles[i][1]));
-        }
-
-        // 현재 위치 , 상하 좌우 이동 , 최소 값 찍기 ,  간거 찍기 , 퍼들 아닌거
-
-
-        dp(0,0 , visited,0);
-
-
-       lcntList.sort(Comparator.comparing(Point::getX));
-
-
-
-
-
-
-        return answer;
-    }
-
-    private void dp(int x , int y ,boolean visited[][], int len) {
-
-        if(x == M-1 && y == N-1){
-            if(llist.contains(len-1)){
-
-            }else {
-                Point rp = new Point(len-1,1); // 새 경로
-                llist.add(len-1);
-                lcntList.add(rp);
-            }
-//            if(min==0) min =len-1;
-//            else min = Math.min(min,len-1);
-        }
-        else{
-            for (int i = 0; i < 4; i++) {
-                int nx = dx[i] + x; int ny = dy[i] + y;
-                Point np = new Point(nx,ny);
-                if (nx < M && nx >= 0 && ny < N && ny >= 0 && !visited[nx][ny] && !plist.contains(np)) {
-                    visited[nx][ny] = true;
-                    dp(nx, ny, visited ,len+1);
-                    visited[nx][ny] = false;
-                }
-            }
-        }
+    public static int solution(int m, int n, int[][] puddles) { // m이 행  n이 열 , 퍼들은 웅덩이 좌표
+      int dp[][] = new int[m+1][n+1];
+      for(int i=0;i<puddles.length;i++) // 웅덩이 0
+          dp[puddles[i][0]][puddles[i][1]] = -1;
+      dp[1][1]=1;
+      for(int i=1;i<dp.length;i++){
+          for(int j=1;j<dp[i].length;j++){
+              if(dp[i][j]==-1){
+                  dp[i][j] =0;
+              }else{
+                  if(i==1) {//끝쪽
+                    dp[i][j] += dp[i][j-1];
+                  }else{
+                      dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % 1000000007;
+                  }
+              }
+          }
+      }
+      return dp[m][n];
     }
 }
