f6a29a3484c5f5df9c86717c9a121b15c9f73f0d
sujin <sujinee9797@gmail.com>
2021-01-21 03:40:57 +0900

    210121_programmers_NQueen
    백트랙킹

--- src/Complete/programmers/N_Queen.java
+++ src/Complete/programmers/N_Queen.java
@@ -1,39 +1,34 @@
 package Complete.programmers;
-
+/*
+* 
+* 대각선의 열 값은 은 증가율 만큼 빼면 됨!
+* 사소한 실수 주의 (절댓값 붙일 때)
+* */
 public class N_Queen {
     public static int solution(int n) {
-        boolean arr[][] = new boolean[n][n]; // false 로 초기화 ?
-        return dfs(0,arr);
+        int col [] = new int[n];
+        return dfs(0,col);
     }
-
-    static boolean isPossible(int x,int y, boolean arr[][]){
+    static boolean isPossible(int x,int y, int col[]){
         for(int i=0;i<x;i++){
-            // 대각선 체크
-            // 세로 체크
-            if(arr[i][y]) return false;
-            if(y-(x-i)>=0) {
-                if (arr[i][y - (x - i)]) return false;
-            }
-            if(y+(x-i)<arr.length) {
-                if (arr[i][y + (x - i)]) return false;
-            }
+            // 세로 체크, 대각선 체크
+            if((col[i]==y) || ( Math.abs(col[i] - y) == x-i)) return false;
         }
         return true;
     }
-    static int dfs(int num , boolean arr[][]){
+    static int dfs(int num , int col[]){
         int answer = 0;
-        if(num >= arr.length)
+        if(num == col.length)
             return 1;
         else
         {
-            for(int i=0;i<arr[num].length;i++){
-                if(isPossible(num, i , arr)){
+            for(int i=0;i<col.length;i++){
+                if(isPossible(num, i , col)){
                     // 선택
-                    arr[num][i] =true;
-                    answer += dfs(num+1,arr);
+                    col[num] = i;
+                    answer += dfs(num+1,col);
                     // 선택 안한 경우 초기화
-                    arr[num][i] =false;
-
+                    col[num] =0;
                 }
             }
         }
