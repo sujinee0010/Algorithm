701619fbe2f2f5c08529f3069e1ce5a1af5d8771
sujin <sujinee9797@gmail.com>
2021-01-25 15:53:03 +0900

    210125_programmers_큰수만들기
    그리디,stack

--- src/Complete/programmers/makeBigNumber.java
+++ src/Complete/programmers/makeBigNumber.java
@@ -1,21 +1,22 @@
 package Complete.programmers;
 
+import java.util.Stack;
+
 public class makeBigNumber {
     public String solution(String number, int k) {
-        String answer = "";
-        int mdx =0;
-        for(int i =k ;i<number.length();i++) {
-            char max ='0';
-            // max 다음 인덱스
-            for(int j =mdx; j<=i;j++){
-                if(max < number.charAt(j)){
-                    max = number.charAt(j);
-                    mdx = j+1;
-                }
-            }
-            answer+=max;
+        char[] result = new char[number.length() - k];
+        Stack<Character> stack = new Stack();
 
+        for(int i=0;i<number.length();i++){
+            while(!stack.isEmpty() && k>0 && stack.peek()<number.charAt(i)){
+                stack.pop();
+                k--;
+            }
+            stack.push(number.charAt(i));
         }
-        return answer;
+        for(int i=0;i<result.length;i++){
+            result[i]=stack.get(i);
+        }
+        return new String(result);
     }
 }
