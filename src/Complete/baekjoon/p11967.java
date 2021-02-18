a49a503b0dfa5c1d805fe06566283dbf3b27e89f
sujin <sujinee9797@gmail.com>
2021-02-18 17:13:51 +0900

    210218_boj_11967_불켜기
    그래프,BFS

--- src/Complete/baekjoon/p11967.java
+++ src/Complete/baekjoon/p11967.java
@@ -9,11 +9,13 @@ public class p11967 {
         int ans=1;
         int n = sc.nextInt();
         int m = sc.nextInt();
-        int room[][] = new int[n+1][n+1];
+        boolean move[][] = new boolean[n+1][n+1];  // 이동 가능하냐
+        boolean visited[][] = new boolean[n+1][n+1];  // 방문했냐
+        boolean light[][] = new boolean[n+1][n+1]; // 불켜져있냐
 
         ArrayList<int[]> rooms_info[][]= new ArrayList[n+1][n+1];
 
-        for(int i=0;i<m;i++){
+        for(int i=0;i<m;i++) {
             int x = sc.nextInt();
             int y = sc.nextInt();
             int a = sc.nextInt();
@@ -24,57 +26,58 @@ public class p11967 {
 
         }
 
+        light[1][1] = true;
+        visited[1][1] = true;
+        move[1][1] =true;
 
-        PriorityQueue<Point> queue = new PriorityQueue<>((o1, o2) -> room[o2.x][o2.y] - room[o1.x][o1.y]);
+        Queue<Point> queue = new LinkedList<>();
 
         queue.add(new Point(1,1));
-        room[1][1]= 1;
         int dx[] ={1,-1,0,0};
         int dy[] ={0,0,-1,1};
 
+        System.out.println(1 + "," + 1);
         while(!queue.isEmpty()){
-            Point room_xy = (Point)queue.poll();
+            // 방 이동
+            Point room_xy =  queue.poll();
+            int x = room_xy.x; int y = room_xy.y;
+            System.out.println("방 이동" + ": " + x + "," + y);
 
-            System.out.println(room_xy.x + "," + room_xy.y);
 
-            // 켜진 방이냐 1
-            if(room[room_xy.x][room_xy.y]==1) {
-                // 해당 방 방문 처리
-                room[room_xy.x][room_xy.y] = 10;
-
-                // 킬수 있는 방을 킨다
-                if(rooms_info[room_xy.x][room_xy.y]!=null) {
-                    for (int i = 0; i < rooms_info[room_xy.x][room_xy.y].size(); i++) {
-                        int possibles[] = rooms_info[room_xy.x][room_xy.y].get(i); // 킬수 있는 방들 좌표
-                        if (room[possibles[0]][possibles[1]] == 0) { // 불 안킨 방 (0) 일 경우에 만 카운팅
-                            System.out.println("방불 키기" + ": " + possibles[0] + "," + possibles[1]);
-                            room[possibles[0]][possibles[1]] = 1; // 불킴 (1)
+            // 방 불켜기 - 이미 켠방은 안킨다
+            if(rooms_info[x][y]!=null) {
+                for (int i = 0; i < rooms_info[x][y].size(); i++) {
+                    int possibles[] = rooms_info[x][y].get(i); // 킬수 있는 방들 좌표
+                        System.out.println("방불 키기" + ": " + possibles[0] + "," + possibles[1]);
+                        if(!light[possibles[0]][possibles[1]]){
+                            light[possibles[0]][possibles[1]] = true; // 불킴
                             ans += 1;
                         }
                     }
+            }
+
+            // 상하좌우 방 이동 가능 처리
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if (nx >= 1 && nx <= n && ny >= 1 && ny <= n) {
+                    move[nx][ny] = true;
+                    System.out.println("이동할 수 있는 방 처리" + ": " + nx + "," + ny);
                 }
-                // 주변 방을 큐에 넣는다
-                for (int i = 0; i < 4; i++) {
-                    int nx = room_xy.x + dx[i];
-                    int ny = room_xy.y + dy[i];
-                    if (nx >= 1 && nx <= n && ny >= 1 && ny <= n && room[nx][ny]!=10 ) { // 방문 안한 방만
-                        Point tmp = new Point(nx, ny);
+            }
 
-                        if(queue.contains(tmp)) {
-                            System.out.println("삭제");
-                            queue.remove(tmp);
-                        }
-
-
-                        System.out.println("방 추가" + ": " + nx + "," + ny);
-                        queue.add(tmp);
+            // 이동가능 & 방불 켠 방 & 아직 방문 안한 방  큐에 넣기
+            for (int j=1;j<n+1;j++){
+                for (int h=1;h<n+1;h++){
+                    if(!visited[j][h] && light[j][h] && move[j][h]){
+                        System.out.println("적합한 방 큐에 삽입" + ": " + j + "," + h);
+                        visited[j][h]=true; // 방문 처리
+                        queue.add(new Point(j,h));
                     }
                 }
-
             }
-            else{
 
-            }
+
         }
 
 
